7b92e0d6f1ff8f5df1f9fd44608aefbb3d6f74f5
Babek Aliyev <baliyev18013@ada.edu.az>
2024-07-12 18:07:32 +0400

    commit

--- Practice/Main.java
+++ Practice/Main.java
@@ -1,25 +1,17 @@
 
 public class Main {
+    private int x=3;
+    public void printX(){
+        System.out.println(x);
+    }
+}
 
-    public static void run(double d){
-        System.out.println("double version: "+d);
-    }
-    public static void run(int i) { System.out.println("Int version: " + i);
-    }
-    public static void run(byte b) { System.out.println("Byte version: " + b);
-    }
-
-    public static void main(String[] args){
-        byte a=5;
-        short b=10;
-        int c =15;
-        float d = 20.1f;
-        double e = 25.23;
-        Main.run(a);
-        Main.run(b);
-        Main.run(c);
-        Main.run(d);
-        Main.run(e);
+class B extends Main{
+    public int x=2;
+    public static void main(String[] args) {
+        B test=new B();
+        test.printX(); 
+        System.out.println(test.x);
     }
 }
 
