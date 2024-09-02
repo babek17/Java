8b1e50799b5e22acac6deb55723de0943416257f
Babek Aliyev <baliyev18013@ada.edu.az>
2024-09-02 12:24:20 +0400

    First problem commit

---
+++ Leetcode/TwoSum.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target){
+        int[] ans=new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target&&i!=j){
+                    ans[0]=i;
+                    ans[1]=j;
+                    break;
+                } 
+            }
+        }
+        return ans;
+    }
+}
